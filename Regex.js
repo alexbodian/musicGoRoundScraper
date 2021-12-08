ae45050308b1b4dc06d1ec77e141c3cb00fe6cc2
Alex <polarzombies@gmail.com>
2021-12-08 11:23:55 -0500

    added more regex

--- Regex.js
+++ Regex.js
@@ -1,7 +1,28 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
+// * Name of item
+// * Price 
+// * Link to page
+// * Link to image
+// * Condition (Used/New)
+// * Location
+
 const fs = require("fs");
 let text = fs.readFileSync("./Output.txt").toString('utf-8');
-let titleRegEx = /title([\s\S]*?)">/;
-let arr = titleRegEx.exec(text);
-item = (arr[0].split('"'))[1];
-console.log(item);
+
+// Grabs name of the item
+let itemRegEx = /title([\s\S]*?)">/;
+let arr = itemRegEx.exec(text);
+nameItem = (arr[0].split('"'))[1]; 
+console.log(nameItem);
+
+// Grabs price of item
+let priceRegEx = /-0">([\s\S]*?)<\/p><!/g;
+arr = priceRegEx.exec(text);
+nameItem = (arr[0].split(' '))[1]; 
+console.log(nameItem);
+
+// Grabs link to page
+let pageRegEx = /-0">([\s\S]*?)<\/p><!/g;
+arr = pageRegEx.exec(text);
+nameItem = (arr[0].split(' '))[1]; 
+console.log(nameItem);
