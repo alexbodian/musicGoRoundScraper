dd50c3de2005d17028b25d6adfbce730a9371c5c
Alex <polarzombies@gmail.com>
2021-12-22 12:08:09 -0500

    stripped out elements that didn't contain guitars

--- scrapeGoRound.js
+++ scrapeGoRound.js
@@ -148,59 +148,71 @@ function sleep(ms) {
 
   await browser.close();
 
-  // console.log(allGuitars);
-  
+  // console.log(allGuitars.length);
+
+  let allGuitarsActual = [];
+
   for (i = 0; i < allGuitars.length; i++) {
-    console.log(i);
-    console.log(allGuitars[i].length);
-    console.log();
+    if (allGuitars[i].length > 50){
+      allGuitarsActual.push(allGuitars[i]);
+    }
   }
 
-  // for (i = 0; i < allGuitars.length; i++) {
-  //   //   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
-  //   // https://regex101.com/
-  //   // * Name of item
-  //   // * Price 
-  //   // * Link to page
-  //   // * Link to image
-  //   // * Condition (Used/New)
-  //   // * Location
-  //   // console.log(text);
-  //   console.log(i)
-  //   let text = allGuitars[i];
-  //   // Grabs name of the item
-  //   let itemRegEx = /title([\s\S]*?)">/;
-  //   let arr = itemRegEx.exec(text);
-  //   nameItem = (arr[0].split('"'))[1];
-  //   console.log(nameItem);
 
-  //   // Grabs price of item
-  //   let priceRegEx = /-0">([\s\S]*?)<\/p><!/g;
-  //   arr = priceRegEx.exec(text);
-  //   namePrice = (arr[0].split(' '))[1];
-  //   console.log(namePrice);
 
-  //   // Grabs link to page
-  //   let pageRegEx = /="https:\/\/www.musicgoround.com\/product\/([\s\S]*?)">/g;
-  //   arr = pageRegEx.exec(text);
-  //   nameLink = (arr[0].split('='))[1];
-  //   nameLink = (arr[0].split('"'))[1];
-  //   console.log(nameLink);
 
-  //   // // Grabs condition of the item
-  //   // let conditionRegEx = /<small>([\s\S]*?)<\/small><!/g;
-  //   // arr = conditionRegEx.exec(text);
-  //   // nameCondition = arr[1];
-  //   // console.log(nameCondition);
 
-  //   // Grabs location of the item
-  //   let locationRegEx = /n><small>([\s\S]*?)<\//g;
-  //   arr = locationRegEx.exec(text);
-  //   nameLocation = arr[1];
-  //   console.log(nameLocation);
+  for (i = 0; i < allGuitarsActual.length; i++) {
+    //   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
+    // https://regex101.com/
+    // * Name of item
+    // * Price 
+    // * Link to page
+    // * Link to image
+    // * Condition (Used/New)
+    // * Location
+    // console.log(text);
+    console.log(i)
+    let text = allGuitarsActual[i];
+    // Grabs name of the item
+    let itemRegEx = /title([\s\S]*?)">/;
+    let arr = itemRegEx.exec(text);
+    let nameItem = (arr[0].split('"'))[1];
+    console.log(nameItem);
+
+    // Grabs price of item
+    let priceRegEx = /-0">([\s\S]*?)<\/p><!/g;
+    arr = priceRegEx.exec(text);
+    let namePrice = (arr[0].split(' '))[1];
+    console.log(namePrice);
+
+    // Grabs link to page
+    let pageRegEx = /="https:\/\/www.musicgoround.com\/product\/([\s\S]*?)">/g;
+    arr = pageRegEx.exec(text);
+    let nameLink = (arr[0].split('='))[1];
+    nameLink = (arr[0].split('"'))[1];
+    console.log(nameLink);
+
+    // // Grabs condition of the item
+    // let conditionRegEx = /<small>([\s\S]*?)<\/small><!/g;
+    // arr = conditionRegEx.exec(text);
+    // nameCondition = arr[1];
+    // console.log(nameCondition);
+
+    // Grabs location of the item
+    let locationRegEx = /n><small>([\s\S]*?)<\//g;
+    arr = locationRegEx.exec(text);
+    let nameLocation = arr[1];
+    console.log(nameLocation);
     
-  //   console.log("");
-  // }
+    console.log("");
+
+    fs.appendFile("Output.txt", guitars.toString(), (err) => {
+      // In case of a error throw err.
+      if (err) throw err;
+    });
+
+  }
 
 
 })();
