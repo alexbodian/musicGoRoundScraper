c09729bc0c87840cfe239842e5ad9fcd08d1a95e
Alex Bodian <polarzombies@gmail.com>
2022-01-04 00:51:10 -0500

    trying to make a webpage

---
+++ index.js
@@ -0,0 +1,5 @@
+import JSON from "./guitars.json" assert {type:
+    "json"};
+
+let container = document.getElementsByClassName("product-list")
+// https://stackoverflow.com/questions/56413874/generating-dynamic-cards-html-cards-from-a-javascript-array-and-bootstrap-in-dif
